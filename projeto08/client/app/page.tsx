874096c5840ad26c6ce792b9504674a45df897f5
luisafalcao <luisafalcaovaz@gmail.com>
2024-06-20 21:05:19 -0300

    Conectado à base de dados

--- projeto08/client/app/page.tsx
+++ projeto08/client/app/page.tsx
@@ -4,26 +4,25 @@ import { useEffect, useState } from "react";
 // import PORT from "../../server/server";
 
 export default function Home() {
-  const [message, setMessage] = useState("Loading...");
-  const [people, setPeople] = useState([]);
+  // const [message, setMessage] = useState("Loading...");
+  const [alunos, setAlunos] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:8080`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setMessage(data.message);
-        setPeople(data.people);
+        console.log(data.alunos);
+        setAlunos(data.alunos);
       });
   }, []);
 
   return (
     <main>
-      <h2>{message}</h2>
+      {/* <h2>{message}</h2> */}
       <ul>
-        {people.map((person, index) => (
-          <li key={index}>{person}</li>
-        ))}
+        {alunos.map((aluno, index) => {
+          return <li key={index}>{aluno.name}</li>;
+        })}
       </ul>
     </main>
   );
--- projeto08/server/server.js
+++ projeto08/server/server.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const cors = require("cors")
+const pg = require("pg")
 
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT;
+const port = process.env.DB_PORT;
 
 app.use(cors());
 
+async function bancodedados() {
+    const client = new pg.Client({
+        connectionString: process.env.DB_URL
+    })
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello", people: ["John", "Joe", "Jim"] })
-})
+    await client.connect();
 
-app.listen(PORT, () => {
-    console.log(`Servidor em http://localhost:${PORT}`)
+    const resultado = await client.query("SELECT * FROM alunos;")
+
+    await client.end();
+
+    return resultado.rows
+}
+
+// app.get("/", (req, res) => {
+//     res.json({ message: "Hello", people: ["John", "Joe", "Jim"] })
+// })
+
+app.get("/", async (req, res) => {
+    const alunos = await bancodedados();
+    console.log(alunos)
+    res.send({ alunos });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor em http://localhost:${port}`)
 })
